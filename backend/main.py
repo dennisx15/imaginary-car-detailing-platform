115a171e0a3c80ddc314f069e3080a7e7e4900a2
dennisx15 <dennisalacahanlii@gmail.com>
2026-05-18 18:18:06 -0600

    added frontend controls

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI # toolkit for creating web servers and APIs
 from backend.routes import faq, appointments
 from backend.database.connection import engine
+from fastapi.middleware.cors import CORSMiddleware # this is a security feature that prevents other websites from making requests to our backend without permission. We will configure it to allow requests from our frontend.
 
 app = FastAPI() # this is the web application object
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #this means allow requests from any origin. In production, you would want to specify the exact origin of your frontend, like "http://localhost:3000" or "https://myfrontend.com"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/") # this is a decorator. When someone sends a GET request to /, the function below will run
 def root():
---
+++ frontend/js/app.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("appointment-form");
+
+form.addEventListener("submit", async (event) => {//This function will run when the submit button is pressed
+
+    event.preventDefault(); // prevents the default form submission behavior, which would cause a page reload
+
+    //extract the values from the form fields and store them in variables
+    const name = document.getElementById("name").value;
+    const service = document.getElementById("service").value;
+
+    
+const response = await fetch( // fetch waits for backend response before moving on to next line of code
+        "http://127.0.0.1:8000/appointments",//this is the backend endpoint we want to send data to
+        {
+            method: "POST", // the kind of http request we want to send
+
+            headers: {
+                "Content-Type": "application/json"
+            }, // some metadata about the request, in this case we are saying we are sending JSON data
+
+            body: JSON.stringify({
+                name: name,
+                service: service
+            }) // this is the actual data we want to send. We create a JavaScript object with the name and service, then convert it to a JSON string with JSON.stringify
+        }
+    );
+
+    const data = await response.json(); // we wait for the response from the backend and then parse it as JSON. The backend will send back a JSON object with a message and the id of the new appointment.
+
+    console.log(data); // we log the response data to the console. In a real application, you might want to update the UI to show a success message or clear the form fields instead of just logging the response.
+
+});
